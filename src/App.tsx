ff597a8c8b499ba4bf97a5e3ca234cbd3875ce50
sontyty <sontaya16071997@gmail.com>
2024-05-23 14:46:59 +0700

    setup eslint

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
 import gradients from './constants/gradients.json'
 
-const App = () => {
+function App() {
   return (
     <>
-      <h1 className='text-3xl font-bold underline'>BG Gradient HUB</h1>
+      <h1 className="text-3xl font-bold underline">BG Gradient HUB</h1>
 
       {/* debug */}
       <pre>{JSON.stringify(gradients, null, 2)}</pre>
