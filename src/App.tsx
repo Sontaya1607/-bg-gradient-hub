37d8cf4d346639bb097ff1c0c3a2b1ca7e399625
sontyty <sontaya16071997@gmail.com>
2024-05-24 10:22:02 +0700

    build bg gradient card

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,50 @@
 import gradients from './constants/gradients.json'
 
+const gradientCardGroup = Object.entries(gradients).map(([title, bgObj], idx) => {
+  const [_, value] = bgObj.bgStyle.split(': ')
+  const myBGStyle: Record<string, string> = {}
+  myBGStyle.backgroundImage = value
+
+  return (
+    <div
+      key={idx}
+      className="rounded overflow-hidden shadow-xl"
+    >
+      <div
+        className="aspect-video !bg-red-300"
+        style={myBGStyle}
+      />
+      <div className="p-4 space-y-1">
+        <h6 className="font-semibold tracking-wider">Name</h6>
+        <span className="text-sm text-gray-900">{title}</span>
+
+        <h6 className="font-semibold tracking-wider">Hex</h6>
+        <div className="text-sm text-gray-900 flex gap-1">
+          <span>#ff9a9e</span>
+          <span>→</span>
+          <span>#fad0c4</span>
+        </div>
+      </div>
+    </div>
+  )
+})
+
 function App() {
   return (
     <>
-      <h1 className="text-3xl font-bold underline">BG Gradient HUB</h1>
+      <main className="min-h-screen font-dm-sans">
+        <div className="container mx-auto">
 
-      {/* debug */}
-      <pre>{JSON.stringify(gradients, null, 2)}</pre>
+          <h1 className="text-3xl font-semibold text-center">BG Gradient HUB</h1>
+
+          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+            {gradientCardGroup}
+          </div>
+
+          {/* debug */}
+          {/* <pre>{JSON.stringify(gradients, null, 2)}</pre> */}
+        </div>
+      </main>
     </>
   )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,21 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
+    './index.html',
+    './src/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'background-100': 'var(--ds-background-100)',
+        'background-200': 'var(--ds-background-200)',
+        'gray-900': 'var(--ds-gray-900)',
+        'gray-1000': 'var(--ds-gray-1000)',
+      },
+      fontFamily: {
+        'dm-sans': ['DM Sans', 'sans-serif'],
+      },
+    },
   },
   plugins: [],
 }
-
